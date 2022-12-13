ce557fa302290ac7b60fd99602f428fdf933fa8f
Fr3kzsy <86102578+FRX132@users.noreply.github.com>
2022-12-13 19:00:56 +0100

    Add files via upload

---
+++ evaluation2-files-group-b/Athlete.java
@@ -0,0 +1,31 @@
+package io.everyonecodes.java.t11_evaluation2.groupb.exercise4;
+import javax.swing.*;
+
+public class Athlete
+    {
+     class Participants
+     {
+        String participation = "participation";
+        String suspended = "suspended";
+        String height = "height";
+        String maximumAthletes = "maximumAthletes";
+
+             for( Participants = )
+         {
+         if (height >= 2.00 )
+            {
+             boolean participation = false;
+            }
+             else (height <= 1.75);
+             {
+                 boolean participation = false;
+             }
+
+
+         }
+
+
+     System.out.println(Participants)
+     }
+
+
---
+++ evaluation2-files-group-b/NumberNameShifter.java
@@ -0,0 +1,9 @@
+package io.everyonecodes.java.t11_evaluation2.groupb.exercise4;
+//Exercise 1
+
+public class NumberNameShifter
+implements academy.everyonecodes.java.evaluation2.exercise1.NumberNamesDictionary;
+
+    {
+     fors(int numbers "" = numbers;++ );
+    }
